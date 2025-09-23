6160ddfdf6f5e5b4f290eb7a9cdece41de50ce60
RichieRann <randipantowx9@gmail.com>
2025-09-23 09:55:47 +0800

    Feat: added new feature, Send Event and Recheive Event.

---
+++ app/Livewire/RechieveEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RechieveEvent extends Component
+{
+    public function render()
+    {
+        return view('livewire.rechieve-event');
+    }
+}
