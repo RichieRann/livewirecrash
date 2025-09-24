f8b618536c72c46503808dae717a9b79d0dcbee4
RichieRann <randipantowx9@gmail.com>
2025-09-24 09:43:22 +0800

    Update: adjust Lazy component class

--- resources/views/livewire/lazy.blade.php
+++ resources/views/livewire/lazy.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-blue-400 text-white font-bold p-2 rounded">
+<div class="bg-blue-400 text-white font-bold p-2 rounded shadow">
     This is the lazy component successfully loaded!
 </div>
